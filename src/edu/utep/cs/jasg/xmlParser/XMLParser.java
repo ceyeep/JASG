a2c2ac2af51e2dfd3a39d5882e8dbd7474a294d1
Cesar Yeep <ceyeep@gmail.com>
2012-11-02 19:00:45 -0600

    Applying copyright header

--- src/edu/utep/cs/jasg/xmlParser/XMLParser.java
+++ src/edu/utep/cs/jasg/xmlParser/XMLParser.java
@@ -1,10 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Cesar Yeep.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the BSD 3-Clause License
+ * ("New BSD" or "BSD Simplified") which accompanies this distribution,
+ * and is available at
+ * http://opensource.org/licenses/BSD-3-Clause
+ * 
+ * Contributors:
+ *     Cesar Yeep - initial API and implementation
+ ******************************************************************************/
 package edu.utep.cs.jasg.xmlParser;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
-
-import org.jdom2.DocType;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
