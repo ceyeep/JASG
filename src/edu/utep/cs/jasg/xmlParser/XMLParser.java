14c32332baacf8e626be9456c00992a40ba1892e
Cesar Yeep <ceyeep@gmail.com>
2012-11-02 18:54:03 -0600

    Adding namespace parsing and XML printer

--- src/edu/utep/cs/jasg/xmlParser/XMLParser.java
+++ src/edu/utep/cs/jasg/xmlParser/XMLParser.java
@@ -3,6 +3,8 @@ package edu.utep.cs.jasg.xmlParser;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+
+import org.jdom2.DocType;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
@@ -10,15 +12,28 @@ import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.XMLOutputter;
 
 public class XMLParser {
+	
+	private String nameSpace = "";
+	
 	public static void main(String[] args) {
 
-		SAXBuilder builder = new SAXBuilder();
-		File xmlFile = new File("jasg.xml");
+		XMLParser parser = new XMLParser();
+		parser.parse();
 
+	}
+	
+	/** Parser XML file. */
+	public void parse(){
 		try {
-
-			Document document = (Document) builder.build(xmlFile);
-			Element rootNode = document.getRootElement();
+			SAXBuilder builder = new SAXBuilder();
+			Document doc = (Document) builder.build(new File("jasg.xml"));
+			Element root = doc.getRootElement();
+			//get namespace
+			nameSpace = root.getChild("nameSpace").getText();
+			
+			//parse different specs used
+			System.out.println(nameSpace);
+			
 
 
 		} catch (IOException io) {
@@ -28,19 +43,36 @@ public class XMLParser {
 		}
 	}
 
+	/** Print XML file using XMLOutputter. */
 	public void printXMLFile(String fileName){
-
 
 		try {
 			// Build the document with SAX and Xerces, no validation
 			SAXBuilder builder = new SAXBuilder();
 			// Create the document
 			Document doc = builder.build(new File(fileName));
+						
 			// Output the document, use standard formatter
 			XMLOutputter fmt = new XMLOutputter();
 			fmt.output(doc, System.out);
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	/** Create a new directory with the specified name. */
+	private void createDirectory(String name){
+		try{
+			String path ="custom/"+ name;
+
+			// Create one directory
+			boolean success = (new File(path)).mkdirs();
+			if (success) {
+				System.out.println("Directory: " + path + " created");
+			}  
+
+		}catch (Exception e){//Catch exception if any
+			System.err.println("Error: " + e.getMessage());
 		}
 	}
 }
