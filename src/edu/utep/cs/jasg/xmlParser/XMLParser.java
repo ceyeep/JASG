b787b8b819cb017aef450978e2259b3b37940f31
Cesar Yeep <ceyeep@gmail.com>
2012-11-02 20:24:43 -0600

    Adding element parser methods

--- src/edu/utep/cs/jasg/xmlParser/XMLParser.java
+++ src/edu/utep/cs/jasg/xmlParser/XMLParser.java
@@ -13,33 +13,78 @@ package edu.utep.cs.jasg.xmlParser;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.XMLOutputter;
 
+import edu.utep.cs.jasg.fileGenerator.MainFileFactory;
+
 public class XMLParser {
 	
 	private String nameSpace = "";
+	private MainFileFactory fileFactory = new MainFileFactory();
 	
+	//Main method for testing purposes only
 	public static void main(String[] args) {
 
 		XMLParser parser = new XMLParser();
-		parser.parse();
+		parser.parse("jasg.xml");
 
 	}
 	
+	//TODO: create subparsing of elements (e.g. parsing rules) pass List to methods.
 	/** Parser XML file. */
-	public void parse(){
+	public void parse(String fileName){
 		try {
 			SAXBuilder builder = new SAXBuilder();
-			Document doc = (Document) builder.build(new File("jasg.xml"));
+			Document doc = (Document) builder.build(new File(fileName));
 			Element root = doc.getRootElement();
-			//get namespace
-			nameSpace = root.getChild("nameSpace").getText();
 			
-			//parse different specs used
+			//create a new namespace
+			nameSpace = root.getChild("nameSpace").getText();
+			createDirectory(nameSpace);
+			
+
+			//get parser elements
+			List<Element> parserElements = root.getChildren("parser");
+			
+			//get scanner elements
+			List<Element> scannerElements = root.getChildren("scanner");
+			
+			//get AST node declarations
+			List<Element> ASTNodeElements = root.getChildren("ASTNodes");
+			
+			//get AST behavior declarations
+			List<Element> ASTBehaviorElements = root.getChildren("ASTBehavior");
+			
+
+			
+			//parse parser elements
+			if(parserElements != null)
+				if(parserElements.size() > 0)
+					parseParserElements(parserElements);
+			
+			//parse scanner elements
+			if(scannerElements != null)
+				if(scannerElements.size() > 0)
+					parseScannerElements(scannerElements);
+			
+			//parse AST node elements
+			if(ASTNodeElements != null)
+				if(ASTNodeElements.size() > 0)
+					parseASTNodeElements(ASTNodeElements);
+			
+			//parse parser elements
+			if(ASTBehaviorElements != null)
+				if(ASTBehaviorElements.size() > 0)
+					parseASTBehaviorElements(ASTBehaviorElements);
+					
+			
+			
 			System.out.println(nameSpace);
 			
 
@@ -49,6 +94,44 @@ public class XMLParser {
 		} catch (JDOMException jdomex) {
 			System.out.println(jdomex.getMessage());
 		}
+	}
+	
+	//TODO: add file name element to assign a file name to a set of elements
+	
+	/** Parse XML parser elements. */
+	private void parseParserElements(List<Element> parserElements){
+		System.out.println("Parsing parser elements");
+		
+		//Create a new parser file
+		fileFactory.createFile(nameSpace, "parser1", "parser");
+		
+	}
+	
+	/** Parse XML scanner elements. */
+	private void parseScannerElements(List<Element> scannerElements){
+		System.out.println("Parsing scanner elements");
+		
+		//Create a new scanner file
+		
+		
+	}
+	
+	/** Parse XML AST node elements. */
+	private void parseASTNodeElements(List<Element> ASTNodeElements){
+		System.out.println("Parsing AST node elements");
+		
+		//Create a new AST file
+		
+		
+	}
+	
+	/** Parse XML AST behavior elements. */
+	private void parseASTBehaviorElements(List<Element> ASTBehaviorElements){
+		System.out.println("Parsing AST behavior elements");
+		
+		//Create a new jrag or jadd file
+		
+		
 	}
 
 	/** Print XML file using XMLOutputter. */
@@ -83,4 +166,6 @@ public class XMLParser {
 			System.err.println("Error: " + e.getMessage());
 		}
 	}
+	
+
 }
